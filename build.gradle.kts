0a29824240d25a59061ab9669f21620caf73afc4
MrXiaoM <coolxiaom95@gmail.com>
2025-06-19 22:11:49 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,18 +12,12 @@ val targetJavaVersion = 8
 val shadowGroup = "top.mrxiaom.sweet.actions.libs"
 
 repositories {
-    mavenLocal()
     mavenCentral()
-    if (Locale.getDefault().country == "CN") {
-        maven("https://maven.fastmirror.net/repositories/minecraft/")
-    }
     maven("https://repo.codemc.io/repository/maven-public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://jitpack.io")
     maven("https://repo.rosewooddev.io/repository/public/")
-    maven("https://s01.oss.sonatype.org/content/groups/public/")
-    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 @Suppress("VulnerableLibrariesLocal")
@@ -33,12 +27,12 @@ dependencies {
 
     compileOnly("me.clip:placeholderapi:2.11.6")
 
-    implementation("net.kyori:adventure-api:4.17.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.4")
-    implementation("net.kyori:adventure-text-minimessage:4.17.0")
-    implementation("de.tr7zw:item-nbt-api:2.14.1")
+    implementation("net.kyori:adventure-api:4.22.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.4.0")
+    implementation("net.kyori:adventure-text-minimessage:4.22.0")
+    implementation("de.tr7zw:item-nbt-api:2.15.0")
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation("top.mrxiaom:PluginBase:1.3.4")
+    implementation("top.mrxiaom:PluginBase:1.4.7")
 }
 java {
     val javaVersion = JavaVersion.toVersion(targetJavaVersion)
